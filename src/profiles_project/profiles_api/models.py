50d682b3ea4e0b27d252a21c24c0e596937324ec
himanshu041 <singhalhimanshu2015@gmail.com>
2017-08-23 22:54:50 +0530

    Added Custom User Projects ,models , manager and migrations for database

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,6 +1,49 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Hello Django work with our custom user model"""
+    def create_user(self,email,name,password=None):
+        """Create new user profiles object"""
+        if not email:
+            raise ValueError("User must hav an email address")
+        email = self.normalise_email(email)
+        user = self.models(email = email ,name = name)
+        user.set_password(password)
+        user.save(using= self.db)
+        return user
+
+    def create_superuser(self ,email, name, password):
+        """Create and save newsuper user with given details"""
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using = self.db)
+        return user
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Represent a user profile inside our system"""
+    email = models.EmailField(max_length =255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects= UserProfileManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['name']
+
+    def get_full_name(self):
+        """Used to get a user full name"""
+        return self.name
+
+    def get_short_name(self):
+        """Used to get a user short name"""
+        return self.name
+
+    def __str__(self):
+        """Django uses this it needs to convert the objects to string"""
+        return self.email
